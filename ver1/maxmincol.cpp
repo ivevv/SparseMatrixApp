a8b6c9657aa46b29ba75a73d25b6a8484e2e6472
eugenia <55488624+ivevv@users.noreply.github.com>
2021-05-15 00:47:06 +0300

    added 5&6 tasks

---
+++ ver1/maxmincol.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <cstdlib>
+#include <malloc.h>
+#include <limits>
+#include "smatrix.h"
+
+void maxMinCol(node **col, int size, int base_elem)
+{
+	int *colMin;
+	colMin = (int*)malloc(size * sizeof(int));
+	int colCount;
+	int min;
+	node *temp;
+	for (int i = 0; i < size; i++)
+	{
+		temp = col[i]->down;
+		min = std::numeric_limits<int>::max();
+		colCount = 0;
+		if (temp != nullptr)
+		{
+			while (temp != col[i])
+			{
+				if (temp->val < min)
+				{
+					min = temp->val;
+				}
+				colCount++;
+				temp = temp->down;
+			}
+			if (colCount < size)
+			{
+				if (min < base_elem)
+				{
+					colMin[i] = min;
+				}
+				else
+				{
+					colMin[i] = base_elem;
+				}
+			}
+			else
+			{
+				colMin[i] = min;
+			}
+		}
+		else
+		{
+			colMin[i] = base_elem;
+		}
+	}
+	int max = std::numeric_limits<int>::min();
+	int colId;
+	for (int i = 0; i < size; i++)
+	{
+		if (colMin[i] > max)
+		{
+			max = colMin[i];
+			colId = i;
+		}
+	}
+	std::cout << std::endl << max << "(column " << colId << ")" << std::endl;
+}
---
+++ ver1/minmaxrow.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <cstdlib>
+#include <malloc.h>
+#include <limits>
+#include "smatrix.h"
+
+void minMaxRow(node **row, int size, int base_elem)
+{
+	int *rowMax;
+	rowMax = (int*)malloc(size*sizeof(int));
+	int rowCount;
+	int max;
+	node *temp;
+	for (int i = 0; i < size; i++)
+	{
+		temp = row[i]->right;
+		max = std::numeric_limits<int>::min();
+		rowCount = 0;
+		if (temp != nullptr)
+		{
+			while (temp != row[i])
+			{
+				if (temp->val > max)
+				{
+					max = temp->val;
+				}
+				rowCount++;
+				temp = temp->right;
+			}
+			if (rowCount < size)
+			{
+				if (max > base_elem)
+				{
+					rowMax[i] = max;
+				}
+				else
+				{
+					rowMax[i] = base_elem;
+				}
+			}
+			else
+			{
+				rowMax[i] = max;
+			}
+		}
+		else
+		{
+			rowMax[i] = base_elem;
+		}
+	}
+	int min = std::numeric_limits<int>::max();
+	int rowId;
+	for (int i = 0; i < size; i++)
+	{
+		if (rowMax[i] < min)
+		{
+			min = rowMax[i];
+			rowId = i;
+		}
+	}
+	std::cout << std::endl << min << "(row " << rowId << ")" << std::endl;
+}
